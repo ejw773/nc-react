048e783067d3af6315cbc3eaee32c9ff8b55d388
Elijah Wilcott <ejw773@gmail.com>
2021-06-27 18:27:38 -0400

    fixes width setting

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -79,7 +79,7 @@ function RenderPartner({partner}) {
     if (partner) {
         return (
             <React.Fragment>
-                <Media object src={partner.image} alt={partner.name} style={{width: "150px"}} />
+                <Media object src={partner.image} alt={partner.name} width="150" />
                 <Media body className="ml-5 mb-4">
                     <Media heading>
                         {partner.name}
