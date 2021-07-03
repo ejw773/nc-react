cfe95b53f3f6f37d7ead663c9d6683be016f135d
Elijah Wilcott <ejw773@gmail.com>
2021-07-03 18:46:53 -0400

    Completes Week 4 Workshop

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm } from 'react-redux-form';
-import { Modal } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 class CommentForm extends React.Component {
     constructor(props) {
@@ -19,39 +23,74 @@ class CommentForm extends React.Component {
         });
     }
 
+    handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+    }
+
     render () {
         return (
             <div>
-                <Modal isOpen="true" toggle={this.toggleModal}>
-                    <LocalForm>
-                        <div className="form-control">
-                            <Control.select
-                                model=".rating"
-                                options="1-5"
-                                id="rating"
-                                name="rating"
-                            />
-                        </div>
-                        <div className="form-control">
-                            <Control.text
-                                model=".author"
-                                id="author"
-                                name="author"
-                            />
-                        </div>
-                        <div className="form-control    ">
-                            <Control.textarea
-                                model=".text"
-                                rows="6"
-                                id="text"
-                                name="text"
-                            />
-                        </div>
-                        <button type="submit">Submit Comment</button>
-                    </LocalForm>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select
+                                    className="form-control"
+                                    model=".rating"
+                                    id="rating"
+                                    name="rating">
+                                    <option value="5">5</option>
+                                    <option value="4">4</option>
+                                    <option value="3">3</option>
+                                    <option value="2">2</option>
+                                    <option value="1">1</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Author</Label>
+                                <Control.text
+                                    className="form-control"
+                                    model=".author"
+                                    id="author"
+                                    name="author"
+                                    validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors 
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        required: "Required",
+                                        minLength: "Must be at least 2 characters",
+                                        maxLength: "Must be 15 characters or less"
+                                    }}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Text</Label>
+                                <Control.textarea
+                                    className="form-control"
+                                    model=".text"
+                                    rows="6"
+                                    id="text"
+                                    name="text"
+                                />
+                            </div>
+                            <Button type="submit" color="primary">Submit Comment</Button>
+                        </LocalForm>
+                    </ModalBody>
                 </Modal>
-                <Button outline>
-                    <i className="fa fa-lg fa-pencil" aria-hidden="true"> Submit Comment</i>
+                <Button outline color="primary">
+                    <i className="fa fa-lg fa-pencil" aria-hidden="true" onClick={this.toggleModal}> Submit Comment</i>
                 </Button>
             </div>
         )
