ff7b51d3770b26fd16ccbb98baf44aaff65ff937
Elijah Wilcott <ejw773@gmail.com>
2021-07-09 04:58:20 -0400

    completes Redux Thunk

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,7 +8,11 @@ function Home(props) {
         <div className="container">
             <div className="row">
                 <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
+                    <RenderCard 
+                        item={props.campsite}
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
+                    />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.promotion} />
