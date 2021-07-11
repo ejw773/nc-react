5c18e5930ef703c9be5eb9a389af6582e3439451
Elijah Wilcott <ejw773@gmail.com>
2021-07-10 22:36:36 -0400

    completes week 5 workshop

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,6 +3,7 @@ import { Breadcrumb, BreadcrumbItem,
     Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { postFeedback } from '../redux/ActionCreators';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -32,8 +33,7 @@ class Contact extends React.Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.props.postFeedback(values);
     }
 
     render() {
